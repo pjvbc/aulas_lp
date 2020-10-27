a63c8aa6d2862d8713105339fc99c9e96b381c46
root <root@DESKTOP-IRP2I8V.localdomain>
2020-10-27 21:38:26 +0000

    Aulas de LP 1 - git, make, gcc, ide, extensoes

---
+++ 1/a.c
@@ -0,0 +1,23 @@
+/**
+ * @file a.c
+ * @author Paulo Costa
+ * @brief Aula 1 LP
+ * @version 0.1
+ * @date 2020-10-27
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+#include <stdio.h>
+
+/**
+ * @brief main function
+ * 
+ * @param argc arguments count
+ * @param argv arguments values
+ * @return int exit code
+ */
+int main(int argc, char* argv[]) {
+	printf("from wstl\n");
+	return 0;
+}
